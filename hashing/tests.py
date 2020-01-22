df14638952b011f83a543d63eb699ef374fdb53a
Boubacar Barry <29449463+boubaca@users.noreply.github.com>
2020-01-22 16:39:03 -0500

    Initial commit

---
+++ hashing/tests.py
@@ -0,0 +1,75 @@
+import time
+
+from django.test import TestCase
+from selenium import webdriver
+from hashing.forms import HashForm
+from hashing.models import Hash
+import hashlib
+from django.core.exceptions import ValidationError
+
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter hash here',self.browser.page_source)
+
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
+
+    def test_hash_ajax(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        time.sleep(5)
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+
+class UnitTestCase(TestCase):
+
+    def test_home_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_hash_form(self):
+        form = HashForm(data={'text':'hello'})
+        self.assertTrue(form.is_valid)
+
+    def test_hash_func_works(self):
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',text_hash)
+
+    def saveHash(self,text='hello'):
+        hash = Hash()
+        hash.text = text
+        hash.hash = hashlib.sha256(hash.text.encode('utf-8')).hexdigest()
+        hash.save()
+        return hash
+
+    def test_hash_object(self):
+        hash = self.saveHash()
+        getHash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertEqual(hash.text,getHash.text)
+        self.assertEqual(hash.hash,getHash.hash)
+
+    def test_viewing_hash(self):
+        hash = self.saveHash()
+        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertContains(response,'hello')
+
+    def test_bad_input(self):
+        def badHash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824ggggggg'
+            hash.full_clean()
+        self.assertRaises(ValidationError,badHash)
